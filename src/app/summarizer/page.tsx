81b635b600c83f587807246714d7678c501c77c9
Jules Maestro <jlsodai@gmail.com>
2023-05-19 11:11:48 +0000

    add padding around page

--- src/app/summarizer/page.tsx
+++ src/app/summarizer/page.tsx
@@ -3,7 +3,7 @@ import SummarySection from "../sections/SummarySection";
 const page = () => {
 	return (
 		<section className="bg-slate-900">
-			<div className="bg-gradient-to-b from-violet-600/[.15] via-transparent min-h-screen flex items-center">
+			<div className="bg-gradient-to-b from-violet-600/[.15] via-transparent min-h-screen flex items-center px-6">
 				<SummarySection />
 			</div>
 		</section>
